fb7c4930ce260eb62991cd158e7ce1fc364de627
Toma Andrei G <56647464+tandreigaabriel@users.noreply.github.com>
2020-12-13 12:45:34 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//animate smooth scrool
+
+$('#view-work').on('click', function () {
+    const images = $('#images').position().top;
+    $('html,body').animate({
+        scrollTop: images
+    },
+        900)
+});
